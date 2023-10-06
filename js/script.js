a4122d5af66a53a9c1a91352f3abf52af415dd36
giuliamari04 <giuliamari04@gmail.com>
2023-10-06 14:48:41 +0200

    esercizio completo come da consegna

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// consegna
+
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+// variabili
+let nome = prompt('qual è il tuo nome?');
+let cognome = prompt('qual è il tuo cognome?');
+let colorepreferito = prompt('qual è il tuo colore preferito?');
+const fixedNumber=21;
+
+// stampa
+console.log(nome+cognome+colorepreferito+fixedNumber);
