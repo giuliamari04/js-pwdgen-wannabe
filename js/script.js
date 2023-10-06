abb2f7d6b5f277e16161641fd9c4fa59a8c488ab
giuliamari04 <giuliamari04@gmail.com>
2023-10-06 15:08:30 +0200

    versione carina e colorata

--- js/script.js
+++ js/script.js
@@ -6,11 +6,43 @@
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 
-// variabili
-let nome = prompt('qual è il tuo nome?');
-let cognome = prompt('qual è il tuo cognome?');
-let colorepreferito = prompt('qual è il tuo colore preferito?');
-const fixedNumber=21;
+// versione richiesta consegna
 
-// stampa
-console.log(nome+cognome+colorepreferito+fixedNumber);
+// // variabili
+// let nome = prompt('qual è il tuo nome?');
+// let cognome = prompt('qual è il tuo cognome?');
+// let colorepreferito = prompt('qual è il tuo colore preferito?');
+// const fixedNumber=21;
+
+// // stampa
+// console.log(nome+cognome+colorepreferito+fixedNumber);
+
+
+
+// versione carina
+
+document.addEventListener("DOMContentLoaded", function () {
+
+
+    let Nome = document.getElementById("nome");
+    let Cognome = document.getElementById("cognome");
+    let Colore = document.getElementById("colore");
+    const fixedNumber=21;
+    
+    let mostraValoreButton = document.getElementById("mostraValore");
+    let outputDiv = document.getElementById("output");
+    
+    mostraValoreButton.addEventListener("click", function () {
+        let valoreInputNome = Nome.value; 
+        let valoreInputCognome = Cognome.value; 
+        let valoreInputColore = Colore.value; 
+        let outputText = valoreInputNome + valoreInputCognome + valoreInputColore + fixedNumber;
+    
+        outputDiv.textContent = outputText;
+        console.log("La tua password è " + outputText);
+        
+    });
+
+
+    
+    });
